e586c008398cc774ce54ec4b7c3e67884859c451
vetrivel-muthusamy <vetrivel.muthusamy@yahoo.com>
2020-10-15 01:52:45 +0530

    Assessment 2 - Display images

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 "# image-hoster-project" 
+##Assessment 1 - Run the application
+###Instructions:
+Display the 'Image Hoster' message as the heading of the website's homepage. In order to implement this, follow the instructions given below:
+
+1. Complete the code in 'HomeController' class to map the homepage file ('index.html') of the website.
+2. In the 'index.html', change the header title to 'Spring MVC Application - Image Hoster' and the heading of the page to ‘Image Hoster’. This will allow you to observe the output in the browser as shown in the image below:
+
+Web page showing 'Image Hoster' as heading and 'Spring MVC Application - Image Hoster' as the title:
--- src/main/java/ImageHoster/controller/HomeController.java
+++ src/main/java/ImageHoster/controller/HomeController.java
@@ -1,8 +1,13 @@
 package ImageHoster.controller;
 
+import ImageHoster.model.Image;
+import ImageHoster.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -11,8 +16,16 @@ public class HomeController {
         System.out.println("**************************** Home Controller ****************************");
     }
 
+    //Write the code to inject the ImageService bean
+	@Autowired
+    private ImageService imageService;
+	
     @RequestMapping("/")
     public String getAllImages(Model model) {
+        //Call getAllImages() method in ImageService class to get the list of all images
+        //Add the list of images in the model with the key as "images"
+		List<Image> images = imageService.getAllImages();
+        model.addAttribute("images", images);
         return "index";
     }
 }
