21db647d913b92f565b91365d5746b36934ed4a7
vetrivel-muthusamy <vetrivel.muthusamy@yahoo.com>
2020-10-16 21:59:28 +0530

    Assessment 4 - Login

--- src/main/java/ImageHoster/controller/ImageController.java
+++ src/main/java/ImageHoster/controller/ImageController.java
@@ -1,12 +1,14 @@
 package ImageHoster.controller;
 
+import ImageHoster.HardCodedImage;
 import ImageHoster.model.Image;
 import ImageHoster.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -18,8 +20,10 @@ public class ImageController {
     //This method displays all the images in the user home page after successful login
     @RequestMapping("images")
     public String getUserImages(Model model) {
-        //Complete the method
-        //Get all the hard-coded images in the application using getAllImages() method in ImageService class and add them to the model with 'images' as the key
+        List<Image> images = imageService.getAllImages();
+        model.addAttribute("images", images);
         return "images";
     }
+
+
 }
